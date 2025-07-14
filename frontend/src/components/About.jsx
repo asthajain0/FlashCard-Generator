00dedef8ea912aa92fc2cf3298ef00155df9ab20
asthajain-iet <22bcs015@ietdavv.edu.in>
2025-07-14 12:55:08 +0530

    about updated

--- frontend/src/components/About.jsx
+++ frontend/src/components/About.jsx
@@ -10,13 +10,9 @@ const About = () => {
             transition={{ duration: 1, delay: 0.25 }}
           >
         <div className='text-center p-2 md:p-5 text-3xl md:text-6xl md:mt-30 text-white/70'>About</div>
-        <div className='p-2 md:px-10 text-lg md:text-xl mx-0 md:mx-10  text-white/60'>Our Flashgen is designed to make learning faster, smarter, and more engaging. Whether you're a student preparing for exams, a professional brushing up on new skills, or simply someone who loves to learn — this tool helps you turn plain notes into powerful study material.
-
-        With just a few clicks, you can generate interactive flashcards that promote active recall and spaced repetition — two scientifically proven techniques for improving memory retention. The intuitive interface allows you to create, organize, and review your flashcards with ease.
-
-        No more manually writing cards or struggling to stay organized. Our generator streamlines your study process so you can focus on what truly matters: understanding and remembering the content. Whether you're studying on your laptop or revising on the go, this tool adapts to your workflow and helps you stay consistent.
-
-        Start creating smarter flashcards today — and take control of your learning journey.</div>
+        <div className='p-2 md:px-10 text-lg md:text-xl mx-0 md:mx-10  text-white/60'>
+          Flashgen makes learning faster, smarter, and more effective. Whether you're a student, professional, or lifelong learner, it helps you turn notes into interactive flashcards using active recall and spaced repetition. With an easy-to-use interface, you can create, organize, and review cards effortlessly — no more manual work or clutter. Study anywhere, anytime, and stay consistent. Start building smarter flashcards and take charge of your learning.
+        </div>
         </motion.div>
         </div>
   )
